bd2f210c0087c882e87f1924f38c4afa2f54f33c
Reimer Darian <rdmalleza@xetid.cu>
2021-06-14 16:23:28 -0400

    min 34

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import HeaderStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={HeaderStyles.title}>
+        <span>Desarrolo Web</span> News
+      </h1>
+      <p className={HeaderStyles.description}>Esta es la description </p>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>MI Web</title>
-        <meta name='keywords' content='web development, programing'/>
+        <meta name="keywords" content="web development, programing" />
       </Head>
       <h1>Bienvenido rei</h1>
     </div>
-  )
+  );
 }
