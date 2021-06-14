67488583edc2db4786682f8044e2d9b82113e5ae
Reimer Darian <rdmalleza@xetid.cu>
2021-06-14 14:41:50 -0400

    iniciando

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>MI Web</title>
+        <meta name='keywords' content='web development, programing'/>
+      </Head>
+      <h1>Bienvenido rei</h1>
+    </div>
+  )
+}
